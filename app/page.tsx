517a469636823c0b7f1563b72611c6518e81152e
Waqar N <Waqar.N@outlook.com>
2024-02-14 23:15:55 -0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { luisatana } from './ui/font';
+import { luisatana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
